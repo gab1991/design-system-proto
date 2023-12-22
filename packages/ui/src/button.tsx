d0bc995a7294d73bef0fdaff6621c39a6f2197d8
Igor Kornilov <gaber32188@gmail.com>
2023-12-22 13:34:28 +0200

    add parcel build for ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,14 +1,15 @@
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+import s from './button.module.css';
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: React.ReactNode;
 }
 
 export function Button({ children, ...other }: ButtonProps): JSX.Element {
-  return (
-    <button type="button" {...other}>
-      {children}
-    </button>
-  );
+	return (
+		<button className={s.button} type="button" {...other}>
+			{children}
+		</button>
+	);
 }
 
-Button.displayName = "Button";
+Button.displayName = 'Button';
