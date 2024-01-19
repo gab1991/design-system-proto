5e0a834c5daf5fd63b6071334350b0c820640b05
Igor Kornilov <gaber32188@gmail.com>
2024-01-19 11:33:18 +0200

    add new icon

--- apps/docs/CHANGELOG.md
+++ apps/docs/CHANGELOG.md
@@ -1,5 +1,12 @@
 # @repo/docs
 
+## 0.0.3
+
+### Patch Changes
+
+- Updated dependencies
+  - @gaber32188/icons@0.2.0
+
 ## 0.0.2
 
 ### Patch Changes
--- packages/icons/src/components/index.ts
+++ packages/icons/src/components/index.ts
@@ -1,3 +1,4 @@
 /// <reference types="../../../../types/sources.d.ts" />
 export { default as Action } from "./action";
 export { default as Add } from "./add";
+export { default as Admin } from "./admin";
